c475854b049003b572c06bd7c9af40b453945ee3
Siriwanthanapengpanja <142291241+Siriwanthana@users.noreply.github.com>
2023-09-12 12:55:48 +0700

    Add files via upload

---
+++ leture06/Python02.py
@@ -0,0 +1,7 @@
+# คำสั่ง if
+if 7 < 20 ** 2 :
+    print('ara ara')
+    print('sasageyo sasayeyo')
+    print('tatakae tatakae')
+
+    print('eren eren eren')
---
+++ leture06/Python03.py
@@ -0,0 +1,10 @@
+# คำสั่ง if + else
+if 7 < 20 ** 2 :
+    print('ara ara')
+    print('sasageyo sasageyo')
+    print('tatakae tatakae')
+else :
+    print('Ymir')
+    print('Titan')
+
+print('eren eren eren')
