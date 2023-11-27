4ed36ad791f537767713e52d514e811ebdb260ec
Siriwanthanapengpanja <142291241+Siriwanthana@users.noreply.github.com>
2023-11-27 16:43:51 +0700

    Delete Lecture 7 directory

--- Lecture 7/test04.py
+++
@@ -1,7 +0,0 @@
-# Tuple Method   
-    #   0   1   2        3          4         5     6
-var1 = (10, 20, 'Hello', True, [111, 'Wow'], 'มานะ','Hello')
-    #   -6  -5  -4       -3         -2        -1    
-print(var1.index('มานะ'))
-
-print(var1.count('Hello'))
