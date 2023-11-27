b1623ff692d8aa59b2b844c144a0c754c72e716a
Siriwanthanapengpanja <142291241+Siriwanthana@users.noreply.github.com>
2023-11-27 16:45:24 +0700

    Delete lecture 7 directory

--- lecture 7/test08.py
+++
@@ -1,7 +0,0 @@
-var2 = (10, 20, 'Hello', True, (111, 'Wow'), 'มานะ')
-# การเปลี่ยนแปลงค่า เพิ่ม ลบ ข้อมูลใน Tuple
-# list(), tuple()
-varTemp = list(var2)
-varTemp[2] = 'Hi'
-var2 = tuple(varTemp)
-print(var2)
